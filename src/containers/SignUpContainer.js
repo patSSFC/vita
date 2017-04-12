35c746044193c3485c85d6bdc88fe3b71dd6211d
Omri Mor <omri1mor@gmail.com>
2017-04-11 17:38:53 -0700

    added a shouldRedirect state and if else to check if user is logged in

--- src/containers/SignUpContainer.js
+++ src/containers/SignUpContainer.js
@@ -5,34 +5,43 @@ import { ref } from '../config/constants';
 
 
 class SignUpContainer extends React.Component {
-
+  state = {
+    shouldRedirect: false
+  }
   componentDidMount() {
-      console.log(`Component did mount`);
+    console.log(`Component did mount`);
   }
   updateUser = (uid, first, last, username) => {
 
-      console.log(`the uid is: ${uid}`);
-      ref.child(`/users/${uid}/info`)
-          .update({
-              first: first,
-              last: last,
-              username: username
-          });
+    console.log(`the uid is: ${uid}`);
+    ref.child(`/users/${uid}/info`)
+        .update({
+            first: first,
+            last: last,
+            username: username
+        });
   }
   createUser = (userInfo) => {
-      console.log(`createUser invoked...`);
+    console.log(`createUser invoked...`);
 
-      auth(userInfo.email, userInfo.password)
-          .then((user) => {
-              this.updateUser(user.uid, userInfo.first, userInfo.last, userInfo.username)
-          });
+    auth(userInfo.email, userInfo.password)
+        .then((user) => {
+            this.updateUser(user.uid, userInfo.first, userInfo.last, userInfo.username)
+            this.setState({shouldRedirect: true})
+        });
   }
   render() {
+    if (this.state.shouldRedirect) {
+      return (
+         <Redirect to="/onboard" />
+      )
+    } else {
       return (
           <SignUp
               onCreateUser={this.createUser}
           />
       )
+    }
   }
 };
 
