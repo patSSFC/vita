e51a51c230ddc085bbcb93a9c0463c66a9ec3aa9
Pat Yolian <pyolian@gmail.com>
2017-04-17 19:51:51 -0700

    add inputHandle to forms on SKill comp

--- src/components/onboarding/Skills.js
+++ src/components/onboarding/Skills.js
@@ -4,17 +4,14 @@ import SkillsWrapper from './SkillsWrapper'
 const Skills = (props) => {
     return (
         <form>
-            <div>
-                <input type="text" name="skill"/>
-            </div>
             <select>
                 {props.skillsList.map((skill) =>{ return <SkillsWrapper value={skill} key={skill._id} />})}
             </select>
             <div>
-                <input type="number" name="experience"/>
+                <input type="number" name="experience" onChange={props.onInputChange}/>
             </div>
             <div>
-                <input type="number" name="stars"/>
+                <input type="number" name="stars" onChange={props.onInputChange}/>
             </div>
         </form>
     )
